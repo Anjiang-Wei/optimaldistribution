3640acf6fe9871960ecfc4fc6b5b1e55876a4a3d
Anjiang Wei <anjiang@g0001.stanford.edu>
2023-05-10 13:06:06 -0700

    make circuit on sapling pascal-aware

--- language/ppopp24_scripts/build_circuit_sapling.sh
+++ language/ppopp24_scripts/build_circuit_sapling.sh
@@ -6,7 +6,7 @@ mkdir "$1"
 cd "$1"
 
 # Hack: -ffuture 0 is a workaround for blocking on a future with the trace loop
-build_option="-fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -findex-launch 1 -ffuture 0"
+build_option="-fflow 0 -fopenmp 0 -fcuda 1 -fcuda-offline 1 -findex-launch 1 -ffuture 0 -fgpu-arch pascal"
 SAVEOBJ=1 STANDALONE=1 OBJNAME=./circuit $root_dir/../regent.py $root_dir/../examples/circuit_sparse.rg $build_option
 
 cp -v $root_dir/*circuit* .
